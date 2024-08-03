25ddaac51910f9b1bd5902c980af7e5a6bc0ab4e
obayemi <obayemi@ikjpz.com>
2024-08-03 16:47:05 +0200

    feat: now with sounds for added annoyance

--- POUP1/scenes/zanu/scripts/zanu.gd
+++ POUP1/scenes/zanu/scripts/zanu.gd
@@ -33,6 +33,8 @@ func _process(_delta):
 		$"Est-ce-une-boite".position = (Vector2.UP * 50).rotated(self.rotation * -1)
 		$"Est-ce-une-boite".show()
 		$"Est-ce-une-boite/Timer".start()
+		$"Est-ce-une-boite/Sound".play()
+		
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
